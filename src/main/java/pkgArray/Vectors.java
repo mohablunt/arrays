f64ac8775fac604d93fa9b7443bdb5fd4882b3c8
mohablunt <mohablunt@gmail.com>
2022-06-10 01:33:24 +0200

    sfsfesg1.2

--- src/main/java/pkgArray/Vectors.java
+++ src/main/java/pkgArray/Vectors.java
@@ -6,9 +6,10 @@ public class Vectors {
     static Random rnd = new Random();
 
     public int[] crearVector(int max) {
-        int v[] = new int[max];
+        int[] v = new int[max];
         for (int i = 0; i < max; i++)
             v[i] = rnd.nextInt(max);
+
         return v;
     }
 
